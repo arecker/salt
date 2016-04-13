64149e10a1c371bf7fc54cb3e50f2ec46f6603c1
Alex Recker <alex@reckerfamily.com>
2016-04-13 18:47:42 -0500

    Fixes list

--- services/files/configure_postgresql.sh
+++ services/files/configure_postgresql.sh
@@ -5,5 +5,5 @@
 # updates the settings for docker
 CONFIG_PATH=`find /etc/postgresql -name "postgresql.conf" | head -1`
 HBA_PATH=`find /etc/postgresql -name "pg_hba.conf" | head -1`
-grep -q -F "listen_addresses='localhost 172.17.0.1/16'" $CONFIG_PATH || echo "listen_addresses='localhost 172.17.0.1/16'" >> $CONFIG_PATH
+grep -q -F "listen_addresses='localhost,172.17.0.1/16'" $CONFIG_PATH || echo "listen_addresses='localhost,172.17.0.1/16'" >> $CONFIG_PATH
 grep -q -F 'host all all 172.17.0.1/16 trust' $HBA_PATH || echo 'host all all 172.17.0.1/16 trust' >> $HBA_PATH
